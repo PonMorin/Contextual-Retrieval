688260db91e4849cdd165e826b192ae9c2fe18dc
PonMorin <pon.m22@gmail.com>
2024-12-24 21:26:09 -0500

    :hammer: Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from ContextualClass.contextual import ContextualRetrieval
 import os
 
 def load_document(tarPath):
-    documents = ""
+    documents = []
     for root, dirs, files in os.walk(tarPath): 
         for file in files:
             full_path = os.path.join(root, file) 
@@ -15,25 +15,46 @@ def load_document(tarPath):
                 loader = TextLoader(full_path, encoding="utf-8")
                 documents = loader.load()
             elif file.endswith('.md'):
-                with open(full_path, 'r', encoding='utf-8') as f:
-                    documents = f.read()
+                loader = TextLoader(full_path, encoding="utf-8")
+                documents.extend(loader.load())
+                # with open(full_path, 'r', encoding='utf-8') as f:
+                #     documents = f.read()
             elif file.endswith('.pdf'):
                 loader = PyPDFLoader(full_path) 
                 documents = loader.load()
     return documents
 
-tarPath = f"./Doc"
+tarPath = f"./digital_doc"
 document = load_document(tarPath)
+# print(document)
+print("Load Document Successful")
 
 cr = ContextualRetrieval()
 
-# Process the document
-original_chunks, contextualized_chunks = cr.process_document(document)
+for doc in document:
+    print(f"Document: {doc.metadata}")
 
-print('----------------------------')
+    # Process the document
+    original_chunks, contextualized_chunks = cr.process_document(doc)
 
-print(original_chunks[0])
+    print('-------------Original-------------')
 
-print('----------------------------')
+    print(original_chunks[0])
 
-print(contextualized_chunks[0])
+    print('-------------Context--------------')
+
+    print(contextualized_chunks[0])
+
+    print('-------------Original-------------')
+
+    print(original_chunks[5])
+
+    print('-------------Context--------------')
+
+    print(contextualized_chunks[5])
+
+    print('----------------------------')
+
+    original_vectorstore = cr.create_vectoDB(original_chunks, "original")
+    contextualized_vectorstore = cr.create_vectoDB(contextualized_chunks, "context")
+    print(f"Save {doc.metadata} to VectorDB!!")
