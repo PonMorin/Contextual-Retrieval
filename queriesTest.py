75c0e0e7a5924f7d6dbdb9cffc07246155546c03
PonMorin <pon.m22@gmail.com>
2025-02-05 01:22:32 -0500

    :hammer: Update queriesTest.py

--- queriesTest.py
+++ queriesTest.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 from dotenv import dotenv_values
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import OpenAIEmbeddings
@@ -8,6 +9,15 @@ from ContextualClass.contextual import ContextualRetrieval
 config = dotenv_values(".env")
 os.environ["OPENAI_API_KEY"] = config["openai_api"]
 
+# # Load the Chroma database from disk
+# chroma_db = Chroma(persist_directory="doc_Data/context", 
+#                    embedding_function=OpenAIEmbeddings(),
+#                    collection_name="capital")
+
+# # Get the collection from the Chroma database
+# collection = chroma_db.get()
+# print(collection)
+
 # Initialize the Contextual Retrieval system
 cr = ContextualRetrieval()
 
@@ -16,50 +26,65 @@ og_data = "./doc_Data/original"
 context_data = "./doc_Data/context"
 
 # Load vectorstores
-original_vector = Chroma(persist_directory=og_data, embedding_function=OpenAIEmbeddings())
-contextual_vector = Chroma(persist_directory=context_data, embedding_function=OpenAIEmbeddings())
+# original_vector = Chroma(persist_directory=og_data, embedding_function=OpenAIEmbeddings(), collection_name="capital")
+contextual_vector = Chroma(persist_directory=context_data, embedding_function=OpenAIEmbeddings(), collection_name="general")
 
 # Define queries
-queries = [
-    "ชื่อปริญญาและสาขาวิชาของหลักสูตรนี้คืออะไร ผมอยากได้ทั้งชื่อภาษาไทยและภาษาอังกฤษ",
-    "ผมอยากทราบ วัน เวลาในการดำเนินการเรียนการสอน",
-    "จำนวนหน่วยกิตที่ต้องเรียนทั้งหมด ต้องเรียนทั้งหมดกี่หน่วยกิต",
-    "รายวิชาของกลุ่มวิชาสังคมศาสตร์ มีรายวิชาอะไรบ้าง",
-    "อยากทราบ แผนการศึกษา ของคณะและสาขา ปีที่ 3 ภาคการศึกษาที่ 2 ",
-    "ช่วยอธิบายรายวิชาของ วิชาโครงสร้างของระบบคอมพิวเตอร์ ให้หน่อย",
-    
-]
+# queries = [
+#     "ชื่อปริญญาและสาขาวิชาของหลักสูตรนี้คืออะไร ผมอยากได้ทั้งชื่อภาษาไทยและภาษาอังกฤษ",
+#     "ผมอยากทราบ วัน เวลาในการดำเนินการเรียนการสอน",
+#     "จำนวนหน่วยกิตที่ต้องเรียนทั้งหมด ต้องเรียนทั้งหมดกี่หน่วยกิต",
+#     "รายวิชาของกลุ่มวิชาสังคมศาสตร์ มีรายวิชาอะไรบ้าง",
+#     "อยากทราบ แผนการศึกษา ของคณะและสาขา ปีที่ 3 ภาคการศึกษาที่ 2 ",
+#     "ช่วยอธิบายรายวิชาของ วิชาโครงสร้างของระบบคอมพิวเตอร์ ให้หน่อย",
+#     "อยากทราบขั้นตอน เกณฑ์การสำเร็จการศึกษา"
+# ]
+
+# queries = [
+#     "อยากทราบวิธีการสมัครขอรับทุน",
+#     "ทุนการศึกษามีกี่ประเภทและอธิบายแต่ละประเภทให้หน่อย",
+#     "ผมอยากทราบคุณสมบัติและหลักเกณฑ์การขอรับทุนการศึกษา",
+#     "รอบแฟ้มสะสมผลงานสามารถสมัครรับทุนได้มั้ย",
+#     "หลักเกณฑ์การพิจารณาทุนส่งเสริมศักยภาพการศึกษามีอะไรบ้าง",
+#     "หลักเกณฑ์การจ่ายทุนช่วยเหลือการศึกษาสำหรับผู้ขาดแคลนมีอะไรบ้าง",
+#     "อยากทราบหลักเกณฑ์การจ่ายทุนพัฒนาและส่งเสริมศักยภาพผู้เรียน"
+# ]
+
+df = pd.read_excel('/home/s6410301020/SeniorProject/Contextual-Retrieval/ข้อสอบทั่วไป.xlsx')
+queries = df["instruction"]
+
+count = 1
 
 # Open a file to save results
-with open("query_results.txt", "w", encoding="utf-8") as file:
+with open("query_results_v2.txt", "w", encoding="utf-8") as file:
     for query in queries:
-        file.write(f"\nQuery: {query}\n")
-        print(f"\nQuery: {query}")
+        file.write(f"\n{count}. Query: {query}\n")
+        print(f"\n{count}. Query: {query}")
 
         # Retrieve from Original Vectorstore
-        original_vector_results = original_vector.similarity_search(query, k=2)
+        # original_vector_results = original_vector.similarity_search(query, k=3)
 
         # Retrieve from Contextual Vectorstore
-        contextual_vector_results = contextual_vector.similarity_search(query, k=2)
+        contextual_vector_results = contextual_vector.similarity_search(query, k=3)
 
         # Generate answers
-        original_vector_answer = cr.generate_answer(query, [doc.page_content for doc in original_vector_results])
-        contextual_vector_answer = cr.generate_answer(query, [doc.page_content for doc in contextual_vector_results])
+        # original_vector_answer = cr.generate_answer_api(query, [doc.page_content for doc in original_vector_results])
+        contextual_vector_answer = cr.generate_answer_api(query, [doc.page_content for doc in contextual_vector_results])
 
         # Write Original Vectorstore results to file
-        file.write("\nOriginal Vector Search Result:\n")
-        print("\nOriginal Vector Search Result:")
-        for i, doc in enumerate(original_vector_results, 1):
-            result_excerpt = doc.page_content
-            file.write(f"Context: {i}. {result_excerpt}\n")
-            print(f"Context: {i}. {result_excerpt}")
+        # file.write("\nOriginal Vector Search Result:\n")
+        # print("\nOriginal Vector Search Result:")
+        # for i, doc in enumerate(original_vector_results, 1):
+        #     result_excerpt = doc.page_content
+        #     file.write(f"Context: {i}. {result_excerpt}\n")
+        #     print(f"Context: {i}. {result_excerpt}")
 
-        file.write("\nOriginal Vector Search Answer:\n")
-        file.write(original_vector_answer + "\n")
-        file.write("\n" + "-"*50)
-        print("\nOriginal Vector Search Answer")
-        print(original_vector_answer)
-        print("\n" + "-"*50)
+        # file.write("\nOriginal Vector Search Answer:\n")
+        # file.write(original_vector_answer + "\n")
+        # file.write("\n" + "-"*50)
+        # print("\nOriginal Vector Search Answer")
+        # print(original_vector_answer)
+        # print("\n" + "-"*50)
 
         # Write Contextual Vectorstore results to file
         file.write("\nContextual Vector Search Result:\n")
@@ -75,5 +100,7 @@ with open("query_results.txt", "w", encoding="utf-8") as file:
         print("\nContextual Vector Search Answer")
         print(contextual_vector_answer)
         print("\n" + "-"*50)
+
+        count = count + 1
 
 print("Results saved to query_results.txt")
